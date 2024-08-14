19585da30e11c620ed08c43f9940fec47949fb3b
Admin <Admin@DESKTOP-1MI440S>
2024-08-14 14:41:57 +0530

    commit1

---
+++ src/BTS/Script01.java
@@ -0,0 +1,27 @@
+package BTS;
+
+public class Script01 
+{
+	@Test
+public void bubble (){
+	int []arr= {10,20,30,11,2};
+	int no= arr.length;
+	for (int j= 0; j < no-1; j++)
+	{
+	for (int i = 0; i <no-1; i++)
+	{
+	if(arr[i]>arr[i+1])
+	{
+	int	temp=arr[i];
+	arr[i]=arr[i+1];
+	arr[i+1]=temp;
+	}
+	}	
+	}
+	for (int k = 0; k <no; k++)
+	{
+	System.out.println(arr[k]+" ");	
+	}
+	
+}
+}
