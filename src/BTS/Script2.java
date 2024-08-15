a29a296fd9e6fc31506b1b0b05a20a3b21a08bc2
Admin <Admin@DESKTOP-1MI440S>
2024-08-15 14:14:19 +0530

    Commit2

---
+++ src/BTS/Script2.java
@@ -0,0 +1,19 @@
+package BTS;
+
+import org.testng.annotations.Test;
+
+public class Script2 
+{
+@Test
+public void swap () 
+{
+	int a=10;
+	int b=20;
+	int temp=a;
+	a=b;
+	b=temp;
+	System.out.println(a);
+	System.out.println(b);
+	
+}
+}
